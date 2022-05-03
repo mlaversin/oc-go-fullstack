3331767700346e031432e5c1e09c53138548f432
Morgan Laversin <morganlaversin@gmail.com>
2022-05-03 10:24:02 +0200

    connection to database

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
+
+mongoose
+  .connect(
+    "mongodb+srv://name:psw@cluster0.dlpup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"))
 
 app.use(express.json())
 
