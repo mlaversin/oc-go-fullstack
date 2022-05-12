be618b58fe68d500233cdd024465de85cd26eba9
Morgan Laversin <morganlaversin@gmail.com>
2022-05-12 08:47:50 +0200

    refactoring : controllers / routes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
-const app = express()
+
+const stuffRoutes = require('./routes/stuff')
 
 mongoose
   .connect(
@@ -10,6 +11,8 @@ mongoose
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"))
+
+const app = express()
 
 app.use(express.json())
 
@@ -26,33 +29,6 @@ app.use((req, res, next) => {
   next()
 })
 
-app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body)
-  res.status(201).json({ message: "Objet créé !" })
-})
-
-app.get("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ]
-  res.status(200).json(stuff)
-})
+app.use('/api/stuff', stuffRoutes)
 
 module.exports = app
