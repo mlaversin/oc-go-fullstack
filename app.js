513f7f94d8909bd185624a0aa7b4cb8ea5e1f5fb
Morgan Laversin <morganlaversin@gmail.com>
2022-05-12 10:00:08 +0200

    authentication : signup route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 
 const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user')
 
 mongoose
   .connect(
@@ -30,5 +31,6 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/stuff', stuffRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app
