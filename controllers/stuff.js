277f39dafe8178fd6649f9b8cd48d1bc24492ee0
Morgan Laversin <morganlaversin@gmail.com>
2022-05-12 11:37:04 +0200

    delete image file when deleting an object

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require("../models/Thing")
+const fs = require('fs')
 
 exports.createThing = (req, res, next) => {
   const thingObject = JSON.parse(req.body.thing)
@@ -33,31 +34,16 @@ exports.modifyThing = (req, res, next) => {
 }
 
 exports.deleteThing = (req, res, next) => {
-  Thing.findOne({ _id: req.params.id }).then(
-    (thing) => {
-      if (!thing) {
-        return res.status(404).json({
-          error: new Error('Objet non trouvé !')
-        })
-      }
-      if (thing.userId !== req.auth.userId) {
-        return res.status(401).json({
-          error: new Error('Requête non autorisée !'),
-        })
-      }
-      Thing.deleteOne({ _id: req.params.id })
-        .then(() => {
-          res.status(200).json({
-            message: 'Deleted!',
-          })
-        })
-        .catch((error) => {
-          res.status(400).json({
-            error: error,
-          })
-        })
-    }
-  )
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => {
+      const filename = thing.imageUrl.split('/images/')[1]
+      fs.unlink(`images/${filename}`, () => {
+        Thing.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+          .catch((error) => res.status(400).json({ error }))
+      })
+    })
+    .catch((error) => res.status(500).json({ error }))
 }
 
 exports.getOneThing = (req, res, next) => {
